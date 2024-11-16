b45ef456ea8c349e9d146c2b8922c354cd8713a8
DarianByrne <173167730+DarianByrne@users.noreply.github.com>
2024-11-16 19:36:46 +0000

    Updated Main

--- src/Main.java
+++ src/Main.java
@@ -31,21 +31,21 @@ public class Main {
 		System.out.println("Stack: " + myStack);
 
 		ParenthesisChecker checker = new ParenthesisChecker();
-		System.out.println("ParenthesisChecker: " + checker.evaluate("public class Main { public static void main(String[] args) { System.out.println(\"Hello world!\"); } }"));
-		System.out.println("ParenthesisChecker: " + checker.evaluate("public class Main { public static void main(String[] args) { System.out.println(\"Hello world!\"); ) }"));
-		System.out.println("ParenthesisChecker: " + checker.evaluate("public class Main { public static void main(String[] args) { System.out.println(\"Hello world!\");  }"));
+		System.out.print("ParenthesisChecker: " + checker.evaluate("public class Main { public static void main(String[] args) { System.out.println(\"Hello world!\"); } }"));
+		System.out.print(", " + checker.evaluate("public class Main { public static void main(String[] args) { System.out.println(\"Hello world!\"); ) }"));
+		System.out.println(", " + checker.evaluate("public class Main { public static void main(String[] args) { System.out.println(\"Hello world!\");  }"));
 
 		String postfix1 = ReversePolishNotation.convert("(55.5-2)+3");
-		System.out.println("ReversePolishNotation: " + postfix1);
+		System.out.print("ReversePolishNotation: postfix: " + postfix1);
 		String postfix2 = ReversePolishNotation.convert("   (   55.5   -   2   )   +   3");
-		System.out.println("ReversePolishNotation: " + postfix2);
-		System.out.println(postfix1.equals(postfix2));
+		System.out.print(", " + postfix2);
+		System.out.println(", " + postfix1.equals(postfix2));
 
 		double result1 = ReversePolishNotation.evaluate(postfix1);
-		System.out.println("ReversePolishNotation: " + result1);
+		System.out.print("ReversePolishNotation: result: " + result1);
 //		compares generated postfix with hardcoded postfix
 		double result2 = ReversePolishNotation.evaluate("55.5 2-3+");
-		System.out.println("ReversePolishNotation: " + result2);
-		System.out.println(result1 == result2);
+		System.out.print(", " + result2);
+		System.out.println(", " + (result1 == result2));
 	}
 }
